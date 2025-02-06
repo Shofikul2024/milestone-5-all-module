57eff9b0fa7953d645c4317c2174a70ff85eca1d
Shofikul2024 <islamshofi942@gmail.com>
2025-02-06 10:43:31 +0600

    third  commit

--- js/loging.js
+++ js/loging.js
@@ -1,7 +1,25 @@
 // console.log(" button clicking added file ");
 
-
+//  h.w  net theke shikye  hobe     form submit reloding  the page  
 document.getElementById('button-login').addEventListener('click' , function () {
-console.log('loging button clicked') ;
+    event.preventDefault()
+ 
+
+
+const phoneNumber =document.getElementById('phone-number').value ;
+
+
+const pinNumber = document.getElementById('pin-number').value ;
+console.log(phoneNumber ,pinNumber) ;
+
+if (phoneNumber ==='5 ' && pinNumber==='1234') {
+    console.log('you are logged in ') ;
+ window.location.href=''
+
+}
+else {
+alert ( 'wrong phone number or pin') ;
+
+}
 
 })  
