c5ac05838b417a38f5556ef27d3a1e6cb2085b1a
Shofikul2024 <islamshofi942@gmail.com>
2025-02-09 14:47:43 +0600

    added  swap without  temporery variable

--- swap.js
+++ swap.js
@@ -12,7 +12,16 @@
 // console.log( x,y)
 
 
- let x=8 ;
- let y= 5 ;
-   [x ,y]= [ y,x]
-   console.log(x ,y)
+//  let x=8 ;
+//  let y= 5 ;
+//    [x ,y]= [ y,x]
+//    console.log(x ,y)
+
+let  a= 5;
+let  b= 7;
+
+console.log(a,b)
+ const temp =a ;
+ a= b ;
+ b=temp ;
+ console.log( a,b)
