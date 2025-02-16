a76a6cff4172f4d7a4f3f093e816d1b02e1eabdc
Shofikul2024 <islamshofi942@gmail.com>
2025-02-16 23:08:56 +0600

    added jokhon return  korbe   tar akta example   descripetion dewya holo

---
+++ arrow-function.js
@@ -0,0 +1,6 @@
+// const aFunc6 = () =>console.log(` A demo Ecma Script  jeita  ke   short  vabe ES6 bola hoy arrow function `)
+// aFunc6()
+
+
+const aFunc6 =() => console.log(`arrow function means , this function   very short and  clean  ,  it helps   our    time `)
+aFunc6 ()
